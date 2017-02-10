e8e5dc42393354bb83da5da826667978083cfc2c
Ahmad Zaki <zaki.bm@gmail.com>
2017-02-10 12:10:38 +0800

    The Arduino file
    Include Distance Sensor as well

---
+++ zaki_lmic_abp.ino
@@ -0,0 +1,279 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
+ *
+ * Permission is hereby granted, free of charge, to anyone
+ * obtaining a copy of this document and accompanying files,
+ * to do whatever they want with them without any restriction,
+ * including, but not limited to, copying, modification and redistribution.
+ * NO WARRANTY OF ANY KIND IS PROVIDED.
+ *
+ * This example sends a valid LoRaWAN packet with payload "Hello,
+ * world!", using frequency and encryption settings matching those of
+ * the (early prototype version of) The Things Network.
+ *
+ * Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in g1,
+ *  0.1% in g2).
+ *
+ * Change DEVADDR to a unique address!
+ * See http://thethingsnetwork.org/wiki/AddressSpace
+ *
+ * Do not forget to define the radio type correctly in config.h.
+ *
+ * 29 December 2016 Modified by Zaki to cater for RF95 + Arduino 
+ *******************************************************************************/
+
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+#include <NewPing.h>
+
+volatile float averageVcc = 0.0;
+#define TRIGGER_PIN  5
+#define ECHO_PIN     4
+#define MAX_DISTANCE 200
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+
+/************************************* OTAA Section : must fill these ******************************************************************/
+/* This EUI must be in little-endian format, so least-significant-byte
+   first. When copying an EUI from ttnctl output, this means to reverse
+   the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3, 0x70. */
+   
+//static const u1_t PROGMEM APPEUI[8]={ 0x5E, 0x1D, 0x00, 0xF0, 0x7E, 0xD5, 0xB3, 0x70 };
+/*Eg : 70B3D57EF0001D5E as got from TTN dashboard*/
+//void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
+
+/* This should also be in little endian format, see above. */
+//static const u1_t PROGMEM DEVEUI[8]={ 0x69, 0x32, 0x30, 0x89, 0x05, 0xE4, 0x54, 0x00 };
+/*Eg : 0054E40589303269 as got from TTN dashboard */
+//void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
+
+/* This key should be in big endian format (or, since it is not really a
+   number but a block of memory, endianness does not really apply). In
+   practice, a key taken from ttnctl can be copied as-is.
+   The key shown here is the semtech default key. */
+//static const u1_t PROGMEM APPKEY[16] = { 0xFF, 0xDF, 0x9C, 0x9A, 0x03, 0x55, 0xFF, 0xAC, 0xA3, 0xD7, 0xDA, 0x52, 0x02, 0xC8, 0xFC, 0xE2 };
+/*Eg : FFDF9C9A0355FFACA3D7DA5202C8FCE2 as got from TTN dashboard */
+//void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
+
+/**************************************** OTAA Section Ends *********************************************************************************/
+
+
+/********************************* ABP Section : Need to fill these *************************************************************************/
+/* LoRaWAN NwkSKey, network session key
+   This is the default Semtech key, which is used by the prototype TTN
+   network initially. */
+static const PROGMEM u1_t NWKSKEY[16] = { 0xF9, 0xFA, 0xB1, 0x36, 0xFC, 0x86, 0x97, 0xD9, 0x9A, 0x96, 0x28, 0xBC, 0xCB, 0x36, 0x88, 0x3B };
+/*Eg : F9FAB136FC8697D99A9628BCCB36883B as got from TTN dashboard*/
+
+/* LoRaWAN AppSKey, application session key
+   This is the default Semtech key, which is used by the prototype TTN
+   network initially. */
+static const u1_t PROGMEM APPSKEY[16] = { 0xA3, 0x00, 0x75, 0x78, 0xD8, 0x02, 0x90, 0xCD, 0x73, 0x0F, 0x9F, 0x8D, 0x86, 0x40, 0x23, 0x90 };
+/*Eg : A3007578D80290CD730F9F8D86402390 as got from TTN dashboard */
+
+/* LoRaWAN end-device address (DevAddr)
+   See http://thethingsnetwork.org/wiki/AddressSpace */
+static const u4_t DEVADDR = 0x26021C17 ; // <-- Change this address for every node!
+/*Eg : 26021C17 as got from TTN dashboard */
+
+/* These callbacks are only used in over-the-air activation, so they are
+   left empty here (we cannot leave them out completely unless
+   DISABLE_JOIN is set in config.h, otherwise the linker will complain). */
+   
+void os_getArtEui (u1_t* buf) { }
+void os_getDevEui (u1_t* buf) { }
+void os_getDevKey (u1_t* buf) { }
+
+/********************************************* ABP Section Ends ***************************************************************************/
+
+
+//static uint8_t mydata[] = "Hello, mine is ABP!";
+static osjob_t sendjob;
+
+
+/* Schedule TX every this many seconds (might become longer due to duty
+   cycle limitations). */
+const unsigned TX_INTERVAL = 10;
+
+/* Pin mapping */
+const lmic_pinmap lmic_pins = {
+    .nss = 10,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = 9,
+    .dio = {2, 6, 7},
+};
+
+void onEvent (ev_t ev) {
+    Serial.print(os_getTime());
+    Serial.print(": ");
+    switch(ev) {
+        case EV_SCAN_TIMEOUT:
+            Serial.println(F("EV_SCAN_TIMEOUT"));
+            break;
+        case EV_BEACON_FOUND:
+            Serial.println(F("EV_BEACON_FOUND"));
+            break;
+        case EV_BEACON_MISSED:
+            Serial.println(F("EV_BEACON_MISSED"));
+            break;
+        case EV_BEACON_TRACKED:
+            Serial.println(F("EV_BEACON_TRACKED"));
+            break;
+        case EV_JOINING:
+            Serial.println(F("EV_JOINING"));
+            break;
+        case EV_JOINED:
+            Serial.println(F("EV_JOINED"));
+            break;
+        case EV_RFU1:
+            Serial.println(F("EV_RFU1"));
+            break;
+        case EV_JOIN_FAILED:
+            Serial.println(F("EV_JOIN_FAILED"));
+            break;
+        case EV_REJOIN_FAILED:
+            Serial.println(F("EV_REJOIN_FAILED"));
+            break;
+            break;
+        case EV_TXCOMPLETE:
+            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+            if(LMIC.dataLen) {
+                /* data received in rx slot after tx */
+                Serial.print(F("Data Received: "));
+                Serial.write(LMIC.frame+LMIC.dataBeg, LMIC.dataLen);
+                Serial.println();
+            }
+            /* Schedule next transmission */
+            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+            break;
+        case EV_LOST_TSYNC:
+            Serial.println(F("EV_LOST_TSYNC"));
+            break;
+        case EV_RESET:
+            Serial.println(F("EV_RESET"));
+            break;
+        case EV_RXCOMPLETE:
+            /* data received in ping slot */
+            Serial.println(F("EV_RXCOMPLETE"));
+            break;
+        case EV_LINK_DEAD:
+            Serial.println(F("EV_LINK_DEAD"));
+            break;
+        case EV_LINK_ALIVE:
+            Serial.println(F("EV_LINK_ALIVE"));
+            break;
+         default:
+            Serial.println(F("Unknown event"));
+            break;
+    }
+}
+
+void do_send(osjob_t* j){
+    
+    static uint8_t message[7];
+    static uint8_t Distance = 0;
+    
+    /* Check if there is not a current TX/RX job running */
+    if (LMIC.opmode & OP_TXRXPEND) {
+        Serial.println(F("OP_TXRXPEND, not sending"));
+    } else {
+        /* Prepare upstream data transmission at the next possible time. */
+        //LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
+
+        Serial.print("\nDistance: ");
+        uint8_t Distance = sonar.ping_cm();
+        Serial.print(Distance);
+        Serial.println("cm");
+        message[0] = Distance;
+        
+        message[1] =  DEVADDR & 0xff;
+        message[2] = (DEVADDR >>  8) & 0xff;
+        message[3] = (DEVADDR >> 16) & 0xff;
+        message[4] = (DEVADDR >> 24) & 0xff;
+
+        averageVcc = averageVcc + (float) readVcc();
+        int16_t averageVccInt = round(averageVcc);
+        Serial.print(averageVccInt);
+        Serial.print(" mV \n"); 
+        message[5] = highByte(averageVccInt);
+        message[6] = lowByte(averageVccInt);
+        
+        LMIC_setTxData2(1, message, sizeof(message), 0);        
+        Serial.println(F("Packet queued"));
+        /*Print Freq being used*/
+        Serial.println(LMIC.freq);
+        /*Print mV being supplied*/
+        
+        delay(1000);
+        averageVcc = 0;
+
+        
+    }
+    /* Next TX is scheduled after TX_COMPLETE event. */
+}
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println(F("Starting"));   
+    
+    #ifdef VCC_ENABLE
+    /* For Pinoccio Scout boards */
+    pinMode(VCC_ENABLE, OUTPUT);
+    digitalWrite(VCC_ENABLE, HIGH);
+    delay(1000);
+    #endif
+
+    /* LMIC init */
+    os_init();
+    /* Reset the MAC state. Session and pending data transfers will be discarded. */
+    LMIC_reset();
+
+    /****************** ABP Only uses this section *****************************************/
+    /* Set static session parameters. Instead of dynamically establishing a session
+       by joining the network, precomputed session parameters are be provided.*/
+    #ifdef PROGMEM
+    /* On AVR, these values are stored in flash and only copied to RAM
+       once. Copy them to a temporary buffer here, LMIC_setSession will
+       copy them into a buffer of its own again. */
+    uint8_t appskey[sizeof(APPSKEY)];
+    uint8_t nwkskey[sizeof(NWKSKEY)];
+    memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+    memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+    LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
+    #else
+    /* If not running an AVR with PROGMEM, just use the arrays directly */
+    LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
+    #endif
+    /******************* ABP Only Ends ****************************************************/
+
+    /* Disable link check validation */
+    LMIC_setLinkCheckMode(0);
+
+    /* Set data rate and transmit power (note: txpow seems to be ignored by the library) */
+    LMIC_setDrTxpow(DR_SF7,14);
+
+    
+    /* Start job */
+    do_send(&sendjob);
+}
+
+void loop() {
+    os_runloop_once();
+}
+
+long readVcc() {
+  long result;
+  // Read 1.1V reference against AVcc
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA,ADSC));
+  result = ADCL;
+  result |= ADCH<<8;
+  result = 1126400L / result; // Back-calculate AVcc in mV
+  return result;
+}
+
+
